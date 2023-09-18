dd35a7b1d4201494a472a1087524089627bbab2e
quinnCraddock4 <quinncraddock@DESKTOP-UI3TT7Q.localdomain>
2023-09-18 15:33:28 -0500

    added tailwind

--- my-app/webpack.renderer.config.ts
+++ my-app/webpack.renderer.config.ts
@@ -3,11 +3,6 @@ import type { Configuration } from 'webpack';
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
-
 export const rendererConfig: Configuration = {
   module: {
     rules,
