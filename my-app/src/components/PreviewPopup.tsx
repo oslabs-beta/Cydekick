17e2d2135ae1cfe44c79d03402d378764587f844
prrrrreston <122579382+prrrrreston@users.noreply.github.com>
2023-10-11 16:02:29 -0500

    styled button on preview popup

--- my-app/src/components/PreviewPopup.tsx
+++ my-app/src/components/PreviewPopup.tsx
@@ -6,17 +6,17 @@ const fs = window.require('fs');
 const os = window.require('os');
 type PreviewPopupProps = {
   onClose: () => void;
-}
+};
 
 const PreviewPopup: React.FC<PreviewPopupProps> = ({ onClose }) => {
   const [code, setCode] = React.useState('');
   const filePreviewPath = path.join(
     os.tmpdir(),
-    "UserTests",
-    "UserTestFile.cy.js"
+    'UserTests',
+    'UserTestFile.cy.js',
   );
   React.useEffect(() => {
-    setCode(fs.readFileSync(filePreviewPath,'utf-8'));
+    setCode(fs.readFileSync(filePreviewPath, 'utf-8'));
   }, []);
 
   const handleEditorChange = (newValue: string) => {
@@ -29,21 +29,50 @@ const PreviewPopup: React.FC<PreviewPopupProps> = ({ onClose }) => {
   };
 
   return (
-    <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1000 }}>
-      <MonacoEditor
-        width="800"
-        height="600"
-        language="javascript"
-        theme="vs-dark"
-        value={code}
-        options={
-          { 
-            selectOnLineNumbers: true, 
-            autoClosingQuotes: "always",
+    // <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1000 }}>
+    //   <MonacoEditor
+    //     width="800"
+    //     height="600"
+    //     language="javascript"
+    //     theme="vs-dark"
+    //     value={code}
+    //     options={
+    //       {
+    //         selectOnLineNumbers: true,
+    //         autoClosingQuotes: "always",
+    //       }}
+    //     onChange={handleEditorChange}
+    //   />
+    //   <button className="bg-blue-500 text-white font-semibold px-4 py-2 rounded-md mt-4" onClick={handleClose}>Close</button>
+    // </div>
+
+    <div
+      style={{
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 1000,
+      }}>
+      <div style={{ position: 'relative', width: '800px', height: '600px' }}>
+        <MonacoEditor
+          language='javascript'
+          theme='vs-dark'
+          value={code}
+          options={{
+            selectOnLineNumbers: true,
+            autoClosingQuotes: 'always',
           }}
-        onChange={handleEditorChange}
-      />
-      <button onClick={handleClose}>Close</button>
+          onChange={handleEditorChange}
+          width='100%'
+          height='100%'
+        />
+        <button
+          onClick={handleClose}
+          className='bg-gradient-to-b from-primary to-primaryDark text-secondary font-semibold px-4 py-2 rounded-md absolute bottom-4 right-4 transition duration-300 hover:shadow-lg hover:font-bold'>
+          Close
+        </button>
+      </div>
     </div>
   );
 };
